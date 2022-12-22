25ac212ab30ddc067fc02b3a8a82a54b061a878d
LindaMangena <lindajazz42@gmail.com>
2022-12-22 11:11:11 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 
@@ -28,5 +29,9 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
+
+
+
+#endif
 
 
