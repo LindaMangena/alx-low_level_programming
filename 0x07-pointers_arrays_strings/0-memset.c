e8c785e9232be2b85f458368eb636e4e9acda78c
LindaMangena <lindajazz42@gmail.com>
2023-03-06 09:30:23 -0800

    a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
-
 /**
- * _memset-Fills the first n bytes of the memory area
- * pointed to by @s with constant byte @c.
- * @s: A pointer to the memory ara to be filled.
- * @c: the character to fill the memory area with.
- * @n: the character number of bytes to be filled.
- * description_memeset: over there
- *Reurn: Apointer to the filled memory area @s.
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
  */
-
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	 unsigned int index;
-	 unsigned char *memory = s, value = c;
+	int i = 0;
 
-	 for (index = 0; index < n; index++) memory[index] = value;
-		return (memory);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
+
